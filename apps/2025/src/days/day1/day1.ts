84c2648d7108b7a6f222e24dbec3fb95a80be254
Piotr 'Ugz' Bielski <piotr.bielski@polishdev.com>
2025-12-28 01:32:12 +0100

    2025: day 1: implement reading data

--- apps/2025/src/days/day1/day1.ts
+++ apps/2025/src/days/day1/day1.ts
@@ -1,6 +1,21 @@
+import { DialOperation, RotationDirection } from "./day1.types";
+
 export const day1 = () => {
   firstStar();
   secondStar();
+};
+
+export const readDataFromString = (data: string): DialOperation[] => {
+  return data.split("\n").filter(line => line !== "").map((line: string, id): DialOperation => {
+    if (line.startsWith("R") && line.startsWith("L")) {
+      throw new Error("2025: Day 1: Wrong direction character");
+    }
+
+    return {
+      direction: line.startsWith("L") ? RotationDirection.Left : RotationDirection.Right,
+      number: parseInt(line.slice(1), 10),
+    };
+  });
 };
 
 const firstStar = () => {};
