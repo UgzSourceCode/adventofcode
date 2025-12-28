43ffcb54ee457ddb5d129940aa94d6b9f9b4c4cc
Piotr 'Ugz' Bielski <piotr.bielski@polishdev.com>
2025-12-28 21:03:59 +0100

    2025: day 2: improve mocks

--- apps/2025/src/days/day2/day2.mocks.ts
+++ apps/2025/src/days/day2/day2.mocks.ts
@@ -1,18 +1,66 @@
-const inputData = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,\n" +
-    "1698522-1698528,446443-446449,38593856-38593862,565653-565659,\n" +
-    "824824821-824824827,2121212118-2121212124";
+import type { Range } from "./day2.types";
 
-const ranges = ["11-22", "95-115", "998-1012", "1188511880-1188511890", "222220-222224", "1698522-1698528", "446443-446449", "38593856-38593862"];
+const inputData =
+  "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,\n" +
+  "1698522-1698528,446443-446449,38593856-38593862,565653-565659,\n" +
+  "824824821-824824827,2121212118-2121212124";
+
+const stringRanges = [
+  "11-22",
+  "95-115",
+  "998-1012",
+  "1188511880-1188511890",
+  "222220-222224",
+  "1698522-1698528",
+  "446443-446449",
+  "38593856-38593862",
+];
+
+const ranges: Range[] = [
+  {
+    low: 11,
+    high: 22,
+  },
+  {
+    low: 95,
+    high: 115,
+  },
+  {
+    low: 998,
+    high: 1012,
+  },
+  {
+    low: 1188511880,
+    high: 1188511890,
+  },
+  {
+    low: 222220,
+    high: 222224,
+  },
+  {
+    low: 1698522,
+    high: 1698528,
+  },
+  {
+    low: 446443,
+    high: 446449,
+  },
+  {
+    low: 38593856,
+    high: 38593862,
+  },
+];
 
 const partialFirstStarResult = [11, 22, 99, 1010, 1188511885, 222222, 446446, 38593859];
 
 const finallyFirstStarResult = 1227775554;
 
 export const day2Mocks = {
-    inputData,
-    ranges,
-    firstStar: {
-        partial: partialFirstStarResult,
-        finallyResult: finallyFirstStarResult,
-    },
+  inputData,
+  stringRanges,
+  ranges,
+  firstStar: {
+    partial: partialFirstStarResult,
+    finallyResult: finallyFirstStarResult,
+  },
 };
