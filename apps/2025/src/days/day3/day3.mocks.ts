a6674080f8481f169eeac4accdd7a677216beefb
Piotr 'Ugz' Bielski <piotr.bielski@polishdev.com>
2026-02-05 00:20:36 +0100

    2025: day 3: added mocks for first star

---
+++ apps/2025/src/days/day3/day3.mocks.ts
@@ -0,0 +1,17 @@
+const inputData =
+  "987654321111111\n" + "811111111111119\n" + "234234234234278\n" + "818181911112111";
+
+const banksData = ["987654321111111", "811111111111119", "234234234234278", "818181911112111"];
+
+const partialFirstStarResult = [98, 89, 78, 92];
+
+const finallyFirstStarResult = 357;
+
+export const day3Mocks = {
+  inputData,
+  banksData,
+  firstStar: {
+    partial: partialFirstStarResult,
+    finallyResult: finallyFirstStarResult,
+  },
+};
