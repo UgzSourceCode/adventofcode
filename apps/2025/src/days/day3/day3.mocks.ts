efbcd5fbe1ec51292c0ada10bdb2d9e46004560c
Piotr 'Ugz' Bielski <piotr.bielski@polishdev.com>
2026-02-05 01:31:56 +0100

    2025: day 3: improved mocks for second star

--- apps/2025/src/days/day3/day3.mocks.ts
+++ apps/2025/src/days/day3/day3.mocks.ts
@@ -4,8 +4,10 @@ const inputData =
 const banksData = ["987654321111111", "811111111111119", "234234234234278", "818181911112111"];
 
 const partialFirstStarResult = [98, 89, 78, 92];
+const partialSecondStarResult = [987654321111, 811111111119, 434234234278, 888911112111];
 
 const finallyFirstStarResult = 357;
+const finallySecondStarResult = 3121910778619;
 
 export const day3Mocks = {
   inputData,
@@ -14,4 +16,8 @@ export const day3Mocks = {
     partial: partialFirstStarResult,
     finallyResult: finallyFirstStarResult,
   },
+  secondStar: {
+    partial: partialSecondStarResult,
+    finallyResult: finallySecondStarResult,
+  }
 };
